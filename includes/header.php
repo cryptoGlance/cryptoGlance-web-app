45784bb3ca4554652d2d1885c55e1d48b073e20a
George Merlocco <george@merloc.co>
2014-02-15 22:23:44 -0500

    Extensively polished the navbar (desktop); Added favicon

--- includes/header.php
+++ includes/header.php
@@ -11,12 +11,11 @@
                <span class="sr-only">Toggle navigation</span>
                <i class="icon icon-dropmenu"></i>
                </button>
-               <a class="navbar-brand" href="index.php"><img src="images/rigwatch-logo-landscape.png" alt="RigWatch" /></a>
+               <a class="navbar-brand" href="index.php"><img src="images/rigwatch-logo-landscape-shadow.png" alt="RigWatch" /></a>
             </div>
             <div class="navbar-collapse collapse">
                <ul class="nav nav-pills navbar-nav">
-                  <li class="active"><a href="index.php"><i class="icon icon-speed"></i> Dashboard</a></li>
-                  <li id="dash-add-panel" class="grad-green"><a title="Add Panel" data-toggle="modal" data-target="#editPool" data-backdrop="static"><i class="icon icon-newwindow"></i></a> </li>
+                  <li class="active topnav-icon"><a id="dash-link" href="index.php"><i class="icon icon-speed"></i> Dashboard</a><a id="dash-add-panel" class="grad-green" title="Add Panel" data-toggle="modal" data-target="#editPool" data-backdrop="static"><i class="icon icon-newtab"></i></a></li>
                   <li class="dropdown">
                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Tools <b class="caret"></b></a>
                      <ul class="dropdown-menu">
@@ -43,9 +42,8 @@
                         <li><a href="https://github.com/scar45/rigwatch" rel="external">Reddit Post</a></li>
                      </ul>
                   </li>
-                  <li id="nav-login-button"><a href="login.php">Login</a></li>
-                  <li id="nav-logout-button"><a href="logout.php">Logout</a></li>
-                  <li class="panel-controls"><a id="collapse-all-panels" href="#" title="Collapse ALL Panels"><i class="icon icon-fastup"></i><span>Collapse All</span></a><a id="expand-all-panels" href="#" title="Expand ALL Panels"><i class="icon icon-fastdown"></i><span>Expand All</span></a></li>
+                  <li id="nav-login-button" class="topnav-icon"><a href="login.php"><i class="icon icon-enteralt"></i> Login</a></li>
+                  <!-- <li id="nav-logout-button" class="topnav-icon"><a href="logout.php"><i class="icon icon-exitalt"></i> Logout</a></li> -->
                </ul>
             </div>
             <!--/.nav-collapse -->
