f371341e328a812471efc706ca794ca7c7335dc5
Stoyvo <stoyanovski.development@gmail.com>
2014-04-16 02:16:28 -0400

    Quick update to new autoloader

--- includes/autoloader.inc.php
+++ includes/autoloader.inc.php
@@ -1,26 +1,8 @@
 <?php
-
-// Initial classes path
-//$classesPath = 'includes/classes';
-//if (defined('CRYPTOGLANCE_PATH')) {
-//    $classesPath = CRYPTOGLANCE_PATH . $classesPath;
-//}
-//
-//$dir = new RecursiveDirectoryIterator($classesPath);
-//
-//foreach(new RecursiveIteratorIterator($dir) as $filepath => $file) {
-//    if (preg_match('/\.php/i', $filepath)) {
-//        require_once($filepath);
-//    }
-//}
-
-//ini_set('display_errors',1);
-//error_reporting(E_ALL);
+ // New Autoloader
 function cgLoader($className) {
     $className = 'classes/' . $className;
-//    if (defined('CRYPTOGLANCE_PATH')) {
-//        $classesPath = CRYPTOGLANCE_PATH;
-//    }
+//    if (defined('CRYPTOGLANCE_PATH')) { $classesPath = CRYPTOGLANCE_PATH; }
     $className = ltrim($className, '\\');
     $fileName  = '';
     $namespace = '';
