c4a3764556e2182d4fcbd15b1c6c13a82e4931c9
Stoyvo <stoyanovski.development@gmail.com>
2014-05-07 02:38:13 -0400

    v1.0.1.40

--- includes/inc.php
+++ includes/inc.php
@@ -25,4 +25,4 @@ $cryptoGlance = new CryptoGlance();
 $settings = $cryptoGlance->getSettings();
 
 //// Current Build:
-define('CURRENT_VERSION', 'v1.0.1.40');
+define('CURRENT_VERSION', 'v1.0.1.40'); 
