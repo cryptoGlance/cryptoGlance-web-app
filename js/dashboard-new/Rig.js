6fcc7d11e7db281d3e9b25ca40d44ee6baf801b2
snypelife <brettmrogerson@gmail.com>
2014-06-06 01:44:51 -0400

    updated TODO, began creating updateDevices method, added more properties to Rig class

--- js/dashboard-new/Rig.js
+++ js/dashboard-new/Rig.js
@@ -1,7 +1,8 @@
 /**
 
   TODO:
-  - continue breaking apart old rigs.js (stopped at line 172)
+  - finish updateDevice's method
+  - continue breaking apart old rigs.js (stopped at line 190)
   - finish creating rest of public methods
   - finish creating rest of private methods
 
@@ -12,12 +13,15 @@
   /*==========  Rig object/class/constructor  ==========*/
 
   var Rig = function (rigId) {
-    this.rigId = rigId
-    this.$rigEl = $('#rig-' + rigId)
-    this.$rigNavEl = this.$rigEl.find('.nav')
+    this.rigId            = rigId
+    this.$rigEl           = $('#rig-' + rigId)
+    this.$rigNavEl        = this.$rigEl.find('.nav')
     this.$rigTabContentEl = this.$rigEl.find('.tab-content')
-    this.$rigTitle = this.$rigEl.find('h1')
-    this.$rigOverviewRow = $('tr[data-rig="'+ rigId +'"]', overviewTable)
+    this.$rigTitle        = this.$rigEl.find('h1')
+    this.$rigOverviewRow  = $('tr[data-rig="'+ rigId +'"]', overviewTable)
+    this.status           = 'green'
+    this.icon             = 'check'
+    this.panel            = ''
   }
 
   /*==========  Public methods  ==========*/
@@ -100,6 +104,7 @@
     }
 
     $summaryContentTab.append(stats)
+    this._updateDevices(devices)
   }
 
 
@@ -125,7 +130,124 @@
             '</div>' +
             '</div>' +
             '</div>'
-  };
+  }
+
+  Rig.prototype._updateDevices = function (devices) {
+    var $summaryContentTabTable = this.$rigTabContentEl.find('#rig-' + rigId + '-summary').find('.table-summary')
+    var $summaryContentTabTableHead = this.$rigTabContentEl.find('thead')
+    var $summaryContentTabTableBody = this.$rigTabContentEl.find('tbody')
+    var removeTable = false
+    $summaryContentTabTable.find('tr').remove()
+
+    if ((typeof devices.GPU != 'undefined' && devices.GPU.length > 0) ||
+         typeof devices.ASC != 'undefined' && devices.ASC.length > 0) {
+        $summaryContentTabTableHead.append('<tr>' +
+                                           '<th></th>' +
+                                           '<th>DEV #</th>' +
+                                           '<th>Temperature</th>' +
+                                           '<th>Hashrate 5s</th>' +
+                                           '<th>Accepted</th>' +
+                                           '<th>Rejected</th>' +
+                                           '<th>Utility</th>' +
+                                           '<th>HW Errors</th>' +
+                                           '</tr>');
+    } else {
+      removeTable = true
+      $summaryContentTabTable.remove()
+    }
+
+    // Update Devices
+    $.each(rig.devs, function (devType, devTypes) {
+        $.each(devTypes, function (devIndex, dev) {
+            // Status colours
+            var status = dev.health;
+            var icon = '';
+            if (dev.enabled == 'N') {
+                status = 'grey';
+                icon = 'ban-circle';
+                rigPanel = 'offline';
+            } else if (status == 'Dead' || (devHWEnabled && dev.hw_errors >= devHWDanger)) {
+                status = 'red';
+                icon = 'danger';
+                rigStatus = 'red';
+                rigIcon = 'danger';
+                rigPanel = 'danger';
+            } else if (status == 'Sick' || (devHWEnabled && dev.hw_errors >= devHWWarning)) {
+                status = 'orange';
+                icon = 'warning-sign';
+                if (rigIcon != 'danger') {
+                    rigStatus = 'orange';
+                    rigIcon = 'warning-sign';
+                    rigPanel = 'warning';
+                }
+            } else if (devHeatDanger <= dev.temperature) {
+                status = 'red';
+                icon = 'hot';
+                if (rigIcon != 'danger' && rigIcon != 'warning-sign') {
+                    rigStatus = 'red';
+                    rigIcon = 'hot';
+                    rigPanel = 'danger';
+                }
+            } else if (devHeatWarning <= dev.temperature) {
+                status = 'orange';
+                icon = 'fire';
+                if (rigIcon != 'danger' && rigIcon != 'warning-sign' && rigIcon != 'hot') {
+                    rigStatus = 'orange';
+                    rigIcon = 'fire';
+                    rigPanel = 'warning';
+                }
+            } else {
+                status = 'green';
+                icon = 'cpu-processor';
+            }
+
+            if (rigPanel != '') {
+                $(rigElm).addClass('panel-' + rigPanel);
+            }
+
+            // add dev to Nav
+            $(rigNavElm).append('<li><a class="rig-'+ rigId +'-'+ devType +'-'+ devIndex +' '+ status +'" href="#rig-'+ rigId +'-'+ devType +'-'+ devIndex +'" data-toggle="tab">'+ devType + devIndex +' <i class="icon icon-'+ icon +'"></i></a></li>');
+            $(rigTabContentElm).find('#rig-'+ rigId +'-'+ devType +'-'+ devIndex).remove();
+            $(rigTabContentElm).append('<div class="tab-pane fade in" id="rig-'+ rigId +'-'+ devType +'-'+ devIndex +'"><div class="panel-body panel-body-stats"></div></div>');
+
+            // Updating DEV Content Tab
+            var devContentTab = $(rigTabContentElm).find('#rig-' + rigId + '-'+ devType +'-' + dev.id).find('.panel-body-stats');
+            $(devContentTab).find('div').remove();
+            $.each(dev, function(k, v) {
+                if (k != 'id' && k != 'enabled') {
+                    if (k == 'temperature') {
+                        v = v + '&deg;C';
+                    } else if (k == 'hashrate_5s' || k == 'hashrate_avg') {
+                        if (v < 1) {
+                            v = (v*1000) + ' KH/S';
+                        } else if (v > 1000) {
+                            v = parseFloat(v/1000).toFixed(2) + ' GH/S';
+                        } else {
+                            v = v + ' MH/S';
+                        }
+                    }
+
+                    $(devContentTab).append('<div class="stat-pair"><div class="stat-value">'+v+'</div><div class="stat-label">'+k.replace(/_|-|\./g, ' ')+'</div></div>');
+                }
+            });
+
+            // Update Summary Page of DEVs
+            if (!removeTable) {
+                if (dev.hashrate_5s < 1) {
+                    dev.hashrate_5s = (dev.hashrate_5s*1000) + ' KH/S';
+                } else if (dev.hashrate_5s > 1000) {
+                    dev.hashrate_5s = parseFloat(dev.hashrate_5s/1000).toFixed(2) + ' GH/S';
+                } else {
+                    dev.hashrate_5s += ' MH/S';
+                }
+
+                $(summaryContentTabTableBody).append('<tr><td><i class="icon icon-'+ icon +' '+status+'"></i></td><td class="'+status+'">'+ devType + dev.id+'</td><td>'+dev.temperature+'&deg;C</td><td>'+dev.hashrate_5s+'</td><td>'+dev.accepted+'</td><td>'+dev.rejected+'</td><td>'+dev.utility+'</td><td>'+dev.hw_errors+'</td></tr>');
+            }
+
+        });
+    });
+
+  }
 
 
   /*==========  Export Rig constructor  ==========*/
