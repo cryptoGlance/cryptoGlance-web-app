dbb2d69f6466ff7b6859caf9a3acae873a01889c
snypelife <brettmrogerson@gmail.com>
2014-06-05 23:47:13 -0400

    migrated button handlers & RigCollection/Rig usage

--- js/dashboard-new/script.js
+++ js/dashboard-new/script.js
@@ -1,10 +1,99 @@
-$(document).ready(function() {
-    var rigs = new Rigs();
-    
-    $('.panel-rig').each(function() {
-        var rigId = $(this).attr('data-id');
-        rigs.add(rigId);
-    });
-    
-    rigs.generateOverview();
-});
+!function ($){
+
+  /*==========  Rig collection init  ==========*/
+  var rigs = new RigCollection()
+
+  $('.panel-rig').each(function(index) {
+    var rigId = this.getAttribute('data-id')
+    var rig = new Rig(rigId)
+    rigs.add(rig)
+  })
+
+  rigs.generateOverview()
+
+
+  /*==========  Button event handlers  ==========*/
+
+  $(document)
+  // Manage Rig
+  .on('click', '.btn-manage-rig', function (evt) {
+    var minerId = this.getAttribute('data-attr')
+    var $manageRig = $('#manageRig')
+
+    $manageRig.attr('data-attr', minerId)
+    $manageRig.find('.rig-name').html($('#rig-' + minerId + ' .panel-title .value').text())
+    $manageRig.find('.btn-details').attr('href', 'rig.php?id=' + minerId)
+
+    prettifyInputs()
+  })
+  // Switch Pools
+  .on('click', '#manageRig .btn-switchpool', function (evt) {
+    var minerId = this.getAttribute('data-attr')
+    var $switchPoolModal = $('#switchPool .checkbox')
+    $switchPoolModal.html('<img src="images/ajax-loader.gif" alt="Loading..." class="ajax-loader" />')
+    $.ajax({
+        type: 'post',
+        data: {
+          type: 'miner',
+          action: 'get-pools',
+          miner: minerId
+        },
+        url: 'ajax.php',
+        dataType: 'json'
+    })
+    .done(function (data) {
+      if (typeof data != 'undefined') {
+        $('#switchPool').attr('data-minerId', minerId);
+        $.each(data, function (v,k) {
+            var poolUrl = k.url.replace(/\:[0-9]{1,4}/, '').slice(poolUrl.indexOf("/") + 2)
+
+            $switchPoolModal.append('<label for="rig'+ minerId +'-pool'+ k.id +'">' +
+                                    '<input type="radio" name="switchPoolList" id="rig'+ minerId +'-pool'+ k.id +'" value="'+ k.id +'">' +
+                                    '<span>'+ poolUrl +'</span>' +
+                                    '</label>')
+            if (k.active == 1) {
+                $('input:radio[id=rig'+ minerId +'-pool'+ k.id +']', $switchPoolModal).prop('checked', true);
+            }
+        });
+
+        prettifyInputs()
+
+        $switchPoolModal.find('.ajax-loader').remove()
+      }
+    })
+  })
+  .on('click', '#switchPool .btn-success', function (evt) {
+    var minerId = this.getAttribute('data-attr')
+    var selectedPoolId = $('input[name=switchPoolList]:checked', '#switchPool .checkbox').val()
+    if (typeof selectedPoolId != 'undefined') {
+      $.ajax({
+        type: 'post',
+        data: {
+          type: 'miners',
+          action: 'switch-pool',
+          miner: minerId,
+          pool: parseInt(selectedPoolId, 10) + 1
+        },
+        url: 'ajax.php',
+        dataType: 'json'
+      })
+      .done(function (data) {
+          ajaxUpdateCall('rig');
+      });
+    }
+  })
+  // Restart
+  .on('click', '#manageRig .btn-restart', function (evt) {
+    var minerId = this.getAttribute('data-attr')
+    $.ajax({
+      type: 'post',
+      data: {
+        type: 'miners',
+        action: 'restart',
+        miner: minerId
+      },
+      url: 'ajax.php',
+      dataType: 'json'
+    })
+  })
+}(window.jQuery)
