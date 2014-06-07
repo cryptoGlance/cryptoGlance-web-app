cbc4dee71e4b2ef9464e4043565b196bf3650274
snypelife <brettmrogerson@gmail.com>
2014-06-06 23:37:45 -0400

    created RigDevice Class file to be used by Rig Class

---
+++ js/dashboard-new/RigDevice.js
@@ -0,0 +1,43 @@
+
+
+
+!function (root, $) {
+
+  /*==========================================================
+  =            RigDevice Class/Object/Constructor            =
+  ==========================================================*/
+
+  var RigDevice = function () {
+
+  }
+
+  /*-----  End of RigDevice Class/Object/Constructor  ------*/
+
+
+  /*================================================
+  =            RigDevice Public Methods            =
+  ================================================*/
+
+
+
+  /*-----  End of RigDevice Public Methods  ------*/
+
+
+  /*=================================================
+  =            RigDevice Private Methods            =
+  =================================================*/
+
+
+
+  /*-----  End of RigDevice Private Methods  ------*/
+
+
+  /*========================================
+  =            RigDevice export            =
+  ========================================*/
+
+  root.RigDevice = RigDevice
+
+  /*-----  End of RigDevice export  ------*/
+
+} (window, window.jQuery)
