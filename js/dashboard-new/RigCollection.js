6cd23a5e788e0bbc296abbbf140778e61b3fea92
snypelife <brettmrogerson@gmail.com>
2014-06-07 01:58:27 -0400

    organized update and start method

--- js/dashboard-new/RigCollection.js
+++ js/dashboard-new/RigCollection.js
@@ -14,6 +14,10 @@
 
   var RigCollection = function () {
     this.collection = []
+    this.overallHashrate = 0
+
+    this.$overview = $('#overview')
+    var overviewTableData = ''
   }
 
   /*-----  End of RigCollection Class/Object/Constructor  ------*/
@@ -23,17 +27,17 @@
   =            RigCollection Public Methods            =
   ====================================================*/
 
-  RigCollection.prototype.add = function (rig) {
-    this.collection.push(rig)
+  RigCollection.prototype.add = function (rigId) {
+    this.collection.push(new Rig(rigId))
   }
 
-  RigCollection.prototype.update = function () {}
+  RigCollection.prototype.start = function() {
+    this.collection.forEach(function (rig) {
+      rig.start()
+    })
+  }
 
-  RigCollection.prototype.generateOverview = function () {
-
-    var overview = $('#overview')
-    var overviewTableData = ''
-
+  RigCollection.prototype.update = function () {
     $.ajax({
       type: 'post',
       data : {
@@ -43,8 +47,39 @@
       url: 'ajax.php',
       dataType: 'json'
     })
+    .done(function (data) {
 
-    $('#overview .panel-body-overview div table tbody').append()
+      $('#overview .panel-body-overview div table tbody').append()
+
+      // Update Overview Panel
+      rig.summary.hashrate_5s = Util.getSpeed(rig.summary.hashrate_5s =)
+
+
+      // update overview
+      if ($rigOverviewRow.length == 0) {
+          $overviewTable.append('<tr data-rig="'+ rigId +'"></tr>');
+          rigOverviewRow = $('tr[data-rig="'+ rigId +'"]', overviewTable)
+      }
+      rigOverviewRow = '<td><i class="icon icon-'+ rigIcon +' '+ rigStatus +'"></i></td>' +
+                       '<td><a href="#rig-'+ rigId +'" class="anchor-offset rig-'+ rigId +' '+ rigStatus +'">'+ $('h1', rigElm).html() +'</a></td>' +
+                       '<td>'+ rig.summary.hashrate_5s +'</td>' +
+                       '<td>'+ rig.summary.active_mining_pool +'</td>' +
+                       '<td>'+ rig.summary.uptime +'</td>'
+      $rigOverviewRow.html(rigOverviewRow)
+
+      $summaryContentTabTable.show();
+
+       // Total amount of hash power
+      for (key in hashrateCollection) {
+          overallHashrate += parseFloat(hashrateCollection[key]);
+      }
+
+      overallHashrate = Util.getSpeed(overallHashrate)
+
+      $('.total-hashrate').html(overallHashrate);
+      document.title = overallHashrate + ' | Dashboard :: cryptoGlance'
+
+    })
   }
 
   /*-----  End of RigCollection Public Methods  ------*/
@@ -68,53 +103,3 @@
   /*-----  End of RigCollection Export  ------*/
 
 }(window, window.jQuery)
-
-$rigNavEl.find('li:eq('+ selectedNav +')').addClass('active');
-$rigTabContentEl.find('.tab-pane:eq('+ selectedNav +')').addClass('active');
-
-$(document).on('shown.bs.tab', 'a[data-toggle="tab"]', function (evt) {
-    var siteLayout = $.cookie('use_masonry_layout');
-    if (siteLayout == 'yes') {
-        initMasonry();
-    }
-});
-
-// Update Overview Panel
-if (rig.summary.hashrate_5s < 1) {
-    rig.summary.hashrate_5s = (rig.summary.hashrate_5s * 1000) + ' KH/S';
-} else if (rig.summary.hashrate_5s > 1000) {
-    rig.summary.hashrate_5s = parseFloat(rig.summary.hashrate_5s / 1000).toFixed(2) + ' GH/S';
-} else {
-    rig.summary.hashrate_5s = parseFloat(rig.summary.hashrate_5s).toFixed(2) + ' MH/S';
-}
-
-// update overview
-if ($rigOverviewRow.length == 0) {
-    $overviewTable.append('<tr data-rig="'+ rigId +'"></tr>');
-    rigOverviewRow = $('tr[data-rig="'+ rigId +'"]', overviewTable)
-}
-rigOverviewRow = '<td><i class="icon icon-'+ rigIcon +' '+ rigStatus +'"></i></td>' +
-                 '<td><a href="#rig-'+ rigId +'" class="anchor-offset rig-'+ rigId +' '+ rigStatus +'">'+ $('h1', rigElm).html() +'</a></td>' +
-                 '<td>'+ rig.summary.hashrate_5s +'</td>' +
-                 '<td>'+ rig.summary.active_mining_pool +'</td>' +
-                 '<td>'+ rig.summary.uptime +'</td>'
-$rigOverviewRow.html(rigOverviewRow)
-
-$summaryContentTabTable.show();
-
- // Total amount of hash power
-    overallHashrate = 0;
-    for (key in hashrateCollection) {
-        overallHashrate += parseFloat(hashrateCollection[key]);
-    }
-    if (overallHashrate < 1) {
-        overallHashrate *= 1000;
-        overallHashrateMetric = 'KH/S';
-    } else if (overallHashrate > 1000) {
-        overallHashrate /= 1000;
-        overallHashrateMetric = 'GH/s';
-    } else {
-        overallHashrateMetric = 'MH/s';
-    }
-    $('.total-hashrate').html(overallHashrate.toFixed(2) + ' <small>'+ overallHashrateMetric +'</small>');
-    document.title = overallHashrate.toFixed(2) + ' ' + overallHashrateMetric + ' | ' + documentTitle
