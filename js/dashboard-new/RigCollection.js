4c53278c919a40026a71eb74002f550d492dc59c
snypelife <brettmrogerson@gmail.com>
2014-06-07 00:52:44 -0400

    updated TODO

--- js/dashboard-new/RigCollection.js
+++ js/dashboard-new/RigCollection.js
@@ -1,7 +1,7 @@
 /**
 
   TODO:
-  - continue breaking apart old rigs.js (stopped at line 190)
+  - finish general structure of RigCollection class
   - setup data synchronization of RigCollection overview
 
 **/
@@ -68,3 +68,31 @@
   /*-----  End of RigCollection Export  ------*/
 
 }(window, window.jQuery)
+
+$(rigNavElm).find('li:eq('+ selectedNav +')').addClass('active');
+        $(rigTabContentElm).find('.tab-pane:eq('+ selectedNav +')').addClass('active');
+
+        $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+            var siteLayout = $.cookie('use_masonry_layout');
+            if (siteLayout == 'yes') {
+                initMasonry();
+            }
+        });
+
+        // Update Overview Panel
+        if (rig.summary.hashrate_5s < 1) {
+            rig.summary.hashrate_5s = (rig.summary.hashrate_5s * 1000) + ' KH/S';
+        } else if (rig.summary.hashrate_5s > 1000) {
+            rig.summary.hashrate_5s = parseFloat(rig.summary.hashrate_5s / 1000).toFixed(2) + ' GH/S';
+        } else {
+            rig.summary.hashrate_5s = parseFloat(rig.summary.hashrate_5s).toFixed(2) + ' MH/S';
+        }
+
+        // update overview
+        if (rigOverviewRow.length == 0) {
+            $(overviewTable).append('<tr data-rig="'+ rigId +'"></tr>');
+            rigOverviewRow = $('tr[data-rig="'+ rigId +'"]', overviewTable)
+        }
+        $(rigOverviewRow).html('<td><i class="icon icon-'+ rigIcon +' '+ rigStatus +'"></i></td><td><a href="#rig-'+ rigId +'" class="anchor-offset rig-'+ rigId +' '+ rigStatus +'">'+ $('h1', rigElm).html() +'</a></td><td>'+ rig.summary.hashrate_5s +'</td><td>'+ rig.summary.active_mining_pool +'</td><td>'+ rig.summary.uptime +'</td>');
+
+        $(summaryContentTabTable).show();
