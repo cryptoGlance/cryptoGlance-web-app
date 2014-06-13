e4c20a380b413c39342b5f40d05596eb307b7dac
snypelife <brettmrogerson@gmail.com>
2014-06-13 02:42:26 -0400

    added method call to show table on update

--- js/dashboard-new/DeviceCollection.js
+++ js/dashboard-new/DeviceCollection.js
@@ -13,6 +13,7 @@
 
   var DeviceCollection = function (rigID) {
     this.rigID = rigID
+    this.$summaryTable = $('#rig-' + rigID + '-summary table')
     this.$summaryTableBody = $('#rig-' + rigID + '-summary table tbody')
     this.collection = []
     this.count = 0
@@ -35,7 +36,7 @@
     this.collection.forEach(function (device, index) {
       deviceSummary += device.update(deviceList[index] || {})
     })
-    // console.log(deviceList, deviceSummary)
+    this.$summaryTable.show()
     this.$summaryTableBody.html(deviceSummary)
   }
 
