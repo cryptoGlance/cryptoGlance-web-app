d7828701afb57de821329cb98215c26d10887ad5
Stoyvo <stoyanovski.development@gmail.com>
2015-01-05 01:54:12 -0500

    fix/mpos-nethashrate
    Nethashrate in MPOS is returned as deca instead of hecto

--- includes/classes/pools/mpos.php
+++ includes/classes/pools/mpos.php
@@ -9,7 +9,7 @@ class Pools_Mpos extends Pools_Abstract {
     protected $_apiKey;
     protected $_userId;
     protected $_type = 'mpos';
-    
+
     // api calls to make
     protected $_actions = array(
         'public',
@@ -31,7 +31,7 @@ class Pools_Mpos extends Pools_Abstract {
             $poolData = array();
             foreach ($this->_actions as $action) {
                 $poolData[$action] = curlCall($this->_apiURL  . '/index.php?page=api&id='. $this->_userId .'&api_key='. $this->_apiKey . '&action=' . $action);
-                
+
                 if ($action == 'getpoolstatus') {
                     $poolData[$action] = $poolData[$action]['getpoolstatus']['data'];
                 } else if ($action == 'getblockstats') {
@@ -42,27 +42,27 @@ class Pools_Mpos extends Pools_Abstract {
                     $poolData[$action] = $poolData[$action]['getuserstatus']['data'];
                 }
             }
-            
+
             // Data Order
             $data['type'] = $this->_type;
-            
+
             $data['balance'] = $poolData['getuserbalance']['confirmed'];
             $data['unconfirmed_balance'] = $poolData['getuserbalance']['unconfirmed'];
-            
-            $data['network_hashrate'] = formatHashrate($poolData['getpoolstatus']['nethashrate']);
+
+            $data['network_hashrate'] = formatHashrate($poolData['getpoolstatus']['nethashrate']/1000);
 
             $data['pool_hashrate'] = formatHashrate($poolData['getpoolstatus']['hashrate']);
-            
+
             $data['user_hashrate'] = formatHashrate($poolData['getuserstatus']['hashrate']);
-            
+
             $data['pool_workers'] = $poolData['getpoolstatus']['workers'];
             $data['efficiency'] = $poolData['getpoolstatus']['efficiency'] . '%';
             $data['accepted'] = $poolData['public']['shares_this_round'];
             $data['rejected'] = round($poolData['public']['shares_this_round'] - ($poolData['public']['shares_this_round'] * ($poolData['getpoolstatus']['efficiency']/100)));
             $data['difficulty'] = round($poolData['getpoolstatus']['networkdiff'], 5);
-            
+
             $timeSinceLastBlock = formatTimeElapsed($poolData['getpoolstatus']['timesincelast']);
-            
+
             $data['time_since_last_block'] = $timeSinceLastBlock;
             $data['%_of_expected'] = round(($poolData['public']['shares_this_round'] / $poolData['getpoolstatus']['estshares']) * 100, 2) . '%';
             $data['current_block'] = $poolData['getpoolstatus']['currentnetworkblock'];
@@ -70,13 +70,13 @@ class Pools_Mpos extends Pools_Abstract {
             $data['blocks_pool_found'] = $poolData['getblockstats']['TotalValid'];
 
             $data['username'] = $poolData['getuserstatus']['username'];
-            
+
             $data['url'] = $this->_apiURL;
-            
+
             $this->_fileHandler->write(json_encode($data));
             return $data;
         }
-        
+
         return json_decode($this->_fileHandler->read(), true);
     }
 
