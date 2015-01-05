6fe51bd8664f440e44d6572c06334047194c5254
Stoyvo <stoyanovski.development@gmail.com>
2015-01-05 03:25:32 -0500

    v2.0.0.443-nightly

--- includes/inc.php
+++ includes/inc.php
@@ -31,7 +31,7 @@ $cryptoGlance = new CryptoGlance();
 $settings = $cryptoGlance->getSettings();
 
 //// Current Build:
-define('CURRENT_VERSION', '2.0.0.439-nightly');
+define('CURRENT_VERSION', '2.0.0.443-nightly');
 
 
 // Misc function used throughout cryptoglance
