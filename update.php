ac5263d9f4fdc026d17d57bac75b0ae98053509c
Stoyvo <stoyanovski.development@gmail.com>
2015-01-05 03:22:54 -0500

    fix/update-missing-library
    Path was changed for jquery, forgot that the update page needs to be set
    manually

--- update.php
+++ update.php
@@ -11,8 +11,8 @@ if (!$_SESSION['login_string']) {
 
 session_write_close();
 
-if (isset($_POST['cryptoglance_version']) && 
-    ($_POST['cryptoglance_version'] != CURRENT_VERSION) && 
+if (isset($_POST['cryptoglance_version']) &&
+    ($_POST['cryptoglance_version'] != CURRENT_VERSION) &&
     ($_SERVER['PHP_SELF'] == $_SERVER['REQUEST_URI'])
 ) {
     set_time_limit(0); // Downloading or unzipping might exceed this time limit
@@ -23,24 +23,22 @@ if (isset($_POST['cryptoglance_version']) &&
     $extractedFolder = '';
     // get settings for update type to get
     $settings = $cryptoGlance->getSettings();
-//    $settings['general']['updates']['enabled'] // 1 / 0
-//    $settings['general']['updates']['type'] // release, beta, nightly
 
     // Need this late in the script
     function lensort($a,$b){
         return strlen($a)-strlen($b);
     }
-    
+
     echo "<html>
     <head>
-        <script type=\"text/javascript\" src=\"js/jquery-1.10.2.min.js\"></script>
+        <script type=\"text/javascript\" src=\"js/packages/jquery-1.10.2.min.js\"></script>
         <script type=\"text/javascript\">
             var scrollUpdate = setInterval(function() {
                 $('html, body').animate({ scrollTop: $(document).height() }, 0);
                 if ($('#done').length != 0) {
                     clearInterval(scrollUpdate);
                     $('#done').remove();
-                } 
+                }
             }, 50);
         </script>
         <style type=\"text/css\">
@@ -62,13 +60,13 @@ if (isset($_POST['cryptoglance_version']) &&
     if (ob_get_level() == 0) ob_start();
     if ($settings['general']['updates']['enabled'] == 1) {
         $updateType = $settings['general']['updates']['type'];
-        
+
         echo '==> Starting Update...Please be patient<br />==> Downloading update zip...<br />'; ob_flush(); flush(); sleep(1);
 
         // MAKE FOLDERS -----------------------
-        
+
         echo '==> Creating temporary directory to download update and unzip: '.$updateDir.'<br />'; ob_flush(); flush();
-        
+
         if (!mkdir($currentDir . $updateDir, 0777, true)) {
             echo '==> ERROR: Failed to create the directory: ' . $updateDir . '<br />';  ob_flush(); flush(); sleep(1);
             exit;
@@ -79,7 +77,7 @@ if (isset($_POST['cryptoglance_version']) &&
         // DOWNLOADING -----------------------
 
         echo '==> Downloading: ' . $updateFeed[$updateType]['zip'] . '<br />'; ob_flush(); flush();
-                
+
         $curl = curl_init();
         $fp = fopen($currentDir . $updateDir .'.zip', 'w');
         curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; cryptoGlance ' . CURRENT_VERSION . '; PHP/' . phpversion() . ')');
@@ -104,31 +102,31 @@ if (isset($_POST['cryptoglance_version']) &&
         curl_close($curl); ob_flush(); flush(); sleep(1);
 
         // UNZIP -----------------------
-        
+
         echo '==> Starting unzip process...<br />'; ob_flush(); flush();
-        
-        $zip = new ZipArchive;  
+
+        $zip = new ZipArchive;
         if (!$zip) {
             echo '==> ERROR: Could not create ZipArchive object...<br />'; ob_flush(); flush(); sleep(1);
             exit;
-        } else if($zip->open($currentDir . $updateDir.'.zip') != "true") { 
+        } else if($zip->open($currentDir . $updateDir.'.zip') != "true") {
             echo '==> ERROR: Could not open ' . $file_zip . '<br />'; ob_flush(); flush(); sleep(1);
             exit;
         }
-        
+
         echo '==> Unzipping archive to: '.$updateDir.'<br />'; ob_flush(); flush(); sleep(1);
-        
+
         $zip->extractTo($currentDir . $updateDir);
         $zip->close();
-        
+
         echo '==> Update Unzipped!<br />'; ob_flush(); flush(); sleep(1);
-        
+
         echo '==> Deleting update archive file!<br />'; ob_flush(); flush();
-        
+
         unlink($currentDir . $updateDir.'.zip');
-        
+
         echo '==> Update archive file deleted!<br />'; ob_flush(); flush(); sleep(1);
-        
+
         foreach (new DirectoryIterator($currentDir . $updateDir) as $file) {
             if($file->isDot() || !$file->isDir()) continue;
             if($file->isDir()) $extractedFolder = $updateDir.DIRECTORY_SEPARATOR.$file->getFilename();
@@ -136,12 +134,12 @@ if (isset($_POST['cryptoglance_version']) &&
         if (empty($extractedFolder)) {
             die('==> ERROR: Something bad happened. Extracted Folder is empty!');
         }
-        
+
         // START UPDATE -----------------------
-        
+
         echo '----------<br />';
         echo '==> Updating cryptoGlance...<br />'; ob_flush(); flush(); sleep(1);
-        
+
         echo '==> Deleting old files:<br />'; ob_flush(); flush(); sleep(1);
         $it = new RecursiveDirectoryIterator('.', RecursiveDirectoryIterator::SKIP_DOTS);
         $files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
@@ -185,7 +183,7 @@ if (isset($_POST['cryptoglance_version']) &&
             }
         }
         echo '==> Done deleting old files...<br />'; ob_flush(); flush(); sleep(1);
-        
+
         echo '<br />==> Copying new files: ' . $extractedFolder . '<br />'; ob_flush(); flush();
         $it = new RecursiveDirectoryIterator($currentDir . $extractedFolder, RecursiveDirectoryIterator::SKIP_DOTS);
         $files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
@@ -217,9 +215,9 @@ if (isset($_POST['cryptoglance_version']) &&
             }
         }
         echo '==> Done copying new files...<br />'; ob_flush(); flush(); sleep(1);
-        
+
         // START Cleanup -----------------------
-        
+
         echo '----------<br />';
         echo '==> Cleaning up... <br />'; ob_flush(); flush(); sleep(1);
         echo '==> Deleting update files:<br />'; ob_flush(); flush(); sleep(1);
@@ -270,9 +268,9 @@ if (isset($_POST['cryptoglance_version']) &&
             echo '==> Cannot Delete Folder: ' . $currentDir.'update' . '<br />'; ob_flush(); flush();
         }
         echo '==> Done cleaning up...<br />'; ob_flush(); flush(); sleep(1);
-        
+
         // FINISHED -----------------------
-                    
+
         echo '----------<br />';
         echo '** cryptoGlance was updated successfully! **<br />You can navigate back to the Dashboard now.';
         echo '<div id="done"></div>'; // this is purely for JS to stop scrolling down
@@ -294,7 +292,7 @@ if (isset($_POST['cryptoglance_version']) &&
 $jsArray = array();
 require_once("includes/header.php");
 ?>
-       
+
     <div id="auto-update-wrap" class="container sub-nav">
         <div id="AutoUpdate" class="panel panel-primary panel-no-grid panel-auto-update">
             <h1>Auto-Updater</h1>
@@ -312,16 +310,6 @@ require_once("includes/header.php");
                     <input type="hidden" name="cryptoglance_version" value="<?php echo $_COOKIE['cryptoglance_version'] ?>" />
                     <iframe name="cg_update" src="#" style="display:none;overflow:hidden;" scrolling="auto"></iframe>
                   </form>
-                    
-<!--                  <pre style="display: none;">-->
-<!--==> Starting-->
-<!--==> Target_url: https://github.com/cryptoGlance/cryptoGlance-web-app/archive/development.zip-->
-<!--==> Headers stripped out-->
-<!--==> Downloaded file: https://github.com/cryptoGlance/cryptoGlancecryptoGlance/cryptoGlancecryptoGlance/cryptoGlance-web-app/archive/development.zip-->
-<!--==> Saved as file: downloads/update.zip-->
-<!--==> About to unzip ...-->
-<!--==> Unzipped file to: downloads/update-->
-<!--                  </pre>-->
                 </div>
               </div>
             </div>
@@ -332,7 +320,7 @@ require_once("includes/header.php");
       <?php require_once("includes/footer.php"); ?>
       </div>
       <!-- /page-container -->
-      
+
       <?php require_once("includes/scripts.php"); ?>
    </body>
 </html>
