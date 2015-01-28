2b360da1a94bb0c2014a8128b99c2a96534d7a18
Stoyvo <stoyanovski.development@gmail.com>
2015-01-28 02:43:59 -0500

    v2.0.0.489-beta

--- includes/inc.php
+++ includes/inc.php
@@ -31,7 +31,7 @@ $cryptoGlance = new CryptoGlance();
 $settings = $cryptoGlance->getSettings();
 
 //// Current Build:
-define('CURRENT_VERSION', 'v2.0.0.488-beta');
+define('CURRENT_VERSION', 'v2.0.0.489-beta');
 
 
 // Misc function used throughout cryptoglance
