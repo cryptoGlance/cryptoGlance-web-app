4b195a103c4b2830d0d6670bfe8bb809e3d9ec4d
Stoyvo <stoyanovski.development@gmail.com>
2015-02-15 19:22:37 -0500

    feat/p2pool

--- includes/classes/config/pools.php
+++ includes/classes/config/pools.php
@@ -117,7 +117,14 @@ class Config_Pools extends Config_Abstract {
                 'address' => $address,
                 'coin' => $coin,
             );
-        } else {
+        } else if ($type == 'p2pool' && !empty($address) && !empty($url)) {
+               $pool = array(
+                   'type' => $type,
+                   'name' => ($label ? $label : 'MagicPool'),
+                   'apiurl' => rtrim($url, '/'),
+                   'address' => $address,
+               );
+           } else {
             header("HTTP/1.0 406 Not Acceptable"); // not accepted
             return 'All fields are required on this form.';
         }
