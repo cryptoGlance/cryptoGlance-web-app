31ae920e61b8ec7bc080441f864ebddc98d37b46
Stoyvo <stoyanovski.development@gmail.com>
2015-03-27 11:28:07 -0400

    fix/permission-fix

--- permissionfix.sh
+++ permissionfix.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# cryptoGlance permission fix script for apache2
+# cryptoGlance permission fix script for most linux setups
 
-sudo chown -R www-data *
+sudo chown -R www-data:www-data .
 sudo chmod -R 777 user_data
-sudo service apache2 restart
