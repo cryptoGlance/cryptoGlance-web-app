6f03e0c0a8363a6e9d293885670e3d7b6cc01b2e
Stoyvo <stoyanovski.development@gmail.com>
2015-03-29 18:14:48 -0400

    v2.0.2.767

--- includes/inc.php
+++ includes/inc.php
@@ -31,7 +31,7 @@ $cryptoGlance = new CryptoGlance();
 $settings = $cryptoGlance->getSettings();
 
 //// Current Build:
-define('CURRENT_VERSION', 'v2.0.2.763');
+define('CURRENT_VERSION', 'v2.0.2.767');
 
 // Misc function used throughout cryptoglance
 require_once('functions.php');
