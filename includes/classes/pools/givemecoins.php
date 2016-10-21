2f83a3aa6b750cc9f8fa0aef2538ba0b6967990f
Zdeněk Jurka <zdenek.jurka@jware.cz>
2016-10-21 23:03:08 +0200

    Added extra info for Give Me Coins pool

--- includes/classes/pools/givemecoins.php
+++ includes/classes/pools/givemecoins.php
@@ -7,15 +7,18 @@ class Pools_GiveMeCoins extends Pools_Abstract {
 	
 	// Pool Information
 	protected $_type = 'give-me-coins';
+	protected $_overalDataUrl;
 	
 	public function __construct($params) {
 		parent::__construct($params);
 		$this->_fileHandler = new FileHandler('pools/' . $this->_type . '/'. hash('md4', $this->_apiURL) .'.json');
+		$this->_overalDataUrl = preg_replace('/[^?]*$/', '', $this->_apiURL);
 	}
 	
 	public function update() {
-		if (true || GLOBALS['cached'] == false || $this->_fileHandler->lastTimeModified() >= 30) {
+		if (GLOBALS['cached'] == false || $this->_fileHandler->lastTimeModified() >= 30) {
 			$poolData = curlCall($this->_apiURL);
+			$overalData = curlCall($this->_overalDataUrl);
 	
 			if (empty($poolData)) {
 				return;
@@ -24,6 +27,13 @@ class Pools_GiveMeCoins extends Pools_Abstract {
 			$data['type'] = $this->_type;
 			$data['user_hashrate'] = formatHashrate($poolData['total_hashrate']/1000);
 			$data['username'] = $poolData['username'];
+			
+			$data['pool_hashrate'] = formatHashrate($overalData['hashrate']/1000);
+			$data['pool_workers'] = $overalData['workers'];
+			$data['network_hashrate'] = formatHashrate($overalData['netGhps'] * 1000000);
+			$data['last_block_reward'] = $overalData['last_block_reward'];
+			$data['difficulty'] = $overalData['difficulty'];
+			$data['shares_this_round'] = $overalData['shares_this_round'];
 			
 			$data['confirmed_balance'] = $poolData['confirmed_rewards'];
 			$data['estimated_balance'] = $poolData['round_estimate'];
